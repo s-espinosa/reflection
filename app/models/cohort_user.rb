964349c447c92d256d3e18acfdb8a13c47db52b0
Sal Espinosa <sespinos@gmail.com>
2017-07-02 18:40:44 -0600

    Move student and teaching cohorts to CohortUser scopes

--- app/models/cohort_user.rb
+++ app/models/cohort_user.rb
@@ -3,4 +3,7 @@ class CohortUser < ApplicationRecord
   belongs_to :cohort
 
   enum role: ["student", "instructor"]
+
+  scope :student_cohort, -> { where(role: "student").first.cohort }
+  scope :teaching_cohort, -> { where(role: "instructor").first.cohort }
 end
